ff63904034236a6c69e57a8fa1af927ae759deac
d.tynybekov <d.tynybekov@bankasia.kg>
2026-03-03 16:48:32 +0600

    Project created

---
+++ src/main/java/task/tynybekoddarmanbektesttask/TynybekodDarmanbekTestTaskApplication.java
@@ -0,0 +1,13 @@
+package task.tynybekoddarmanbektesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TynybekodDarmanbekTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TynybekodDarmanbekTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/task/tynybekoddarmanbektesttask/TynybekodDarmanbekTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package task.tynybekoddarmanbektesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TynybekodDarmanbekTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
